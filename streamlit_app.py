f780768f287e5c3c713ea04cc3ea7f42030f8e74
annapuigvert <139990066+annapuigvert@users.noreply.github.com>
2023-07-19 11:16:47 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-streamlit.title('My Mom'''s New Healthy Diner')
+streamlit.title('My Mom'&apos's New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
