c47281f2522c4fee4023ce69d557a4da909d3a6e
annapuigvert <139990066+annapuigvert@users.noreply.github.com>
2023-07-20 15:14:56 -0400

    Update streamlit_app.py
    add stop command

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,6 +35,9 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
+#add a stop command to focus our attention
+streamlit.stop()
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
@@ -47,3 +50,4 @@ add_my_fruit=streamlit.text_input('What fruit would you like to add?','Jackfruit
 streamlit.write('Thanks for adding ', add_my_fruit)
 
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
